6f414b7b4f1befed78ce341670d599989cde4090
johngarcia73 <johngm000@gmail.com>
2025-01-18 22:54:07 -0500

    Added urls

---
+++ frontend/react-project/src/hooks/useFetchQuestions.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { Question } from "../components/Interfaces"; // Asegúrate de importar la interfaz
+
+const useFetchQuestions = (subjectIds: number[]) => {
+  const [questions, setQuestions] = useState<Record<number, Question[]>>({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const questionsData: Record<number, Question[]> = {};
+        for (const subjectId of subjectIds) {
+          try {
+            const response = await axios.get(
+              `http://localhost:8000/api/subject/questions/${subjectId}/`
+            );
+            questionsData[subjectId] = response.data;
+          } catch (err: unknown) {
+            if (err instanceof AxiosError) {
+              if (err.response && err.response.status === 404) {
+                questionsData[subjectId] = []; // Si no se encuentran preguntas, asignar un array vacío
+              } else {
+                throw err; // Para otros errores, lanzarlos
+              }
+            } else {
+              throw err; // Para otros tipos de errores, lanzarlos
+            }
+          }
+        }
+        setQuestions(questionsData);
+      } catch (error) {
+        setError("Error al obtener las preguntas");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (subjectIds.length > 0) {
+      fetchQuestions();
+    }
+  }, [subjectIds]);
+
+  return { questions, loading, error };
+};
+
+export default useFetchQuestions;
