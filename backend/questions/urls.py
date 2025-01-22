588b490b91a4d5ff214c7ba864cf45e00214dba3
VictorH. <pachecovictorh003@gmail.com>
2025-01-22 01:00:06 -0500

    update views

--- backend/questions/urls.py
+++ backend/questions/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from rest_framework import routers
 
 from .views import *
+from .viewss.subjects import *
+from .viewss.topics import *
+from .viewss.questions import *
 
 
 
@@ -15,20 +18,27 @@ router=routers.DefaultRouter()
 urlpatterns =[
     path('subjects/', subject_list),
     path('subject/<int:pk>/',subject_detail),
-    
+    path('subject/question/<int:subject_id>/',subject_questions),
+    path('subject/topics/<int:subject_id>/',subject_topics),
+     
     path('topic/', topic_list),
     path('topic/<int:pk>/',topic_detail),
+    path('topic/subject/<int:topic_id>/',topic_subject),
+    path('topic/questions/<int:topic_id>/',topic_question),
     
     path('question/', question_list),
     path('question/<int:pk>/',question_detail),
+    path('question/topic/<int:question_id>/',question_topic),
     
     path('exam/', exam_list),
     path('exam/<int:pk>/',exam_detail),
+    
+    
     path('teacher/subjects/<int:teacher_id>/',teacher_subjects),
-    path('subject/question/<int:subject_id>/',subject_questions),
-    path('topic/subject/<int:topic_id>/',topic_subject),
-    path('question/topic/<int:question_id>/',question_topic),
-    path('subject/topics/<int:subject_id>/',subject_topics),
-    path('topic/questions/<int:topic_id>/',topic_question),
+   
+    
+    
+    
+    
      
 ]+router.urls
--- backend/questions/views.py
+++ backend/questions/views.py
@@ -22,9 +22,6 @@ from drf_spectacular.utils import extend_schema, extend_schema_view,OpenApiRespo
 #     permission_classes=[permissions.AllowAny]
 #     serializer_class=SubjectSerializer
     
-
-
-
 # @extend_schema_view(
 #     list=extend_schema(tags=['Topic']),
 #     retrieve=extend_schema(tags=['Topic']),
@@ -37,7 +34,6 @@ from drf_spectacular.utils import extend_schema, extend_schema_view,OpenApiRespo
 #     queryset=Topic.objects.all()
 #     permission_classes=[permissions.AllowAny]
 #     serializer_class=TopicSerializer
-
 
 # @extend_schema_view(
 #     list=extend_schema(tags=['Question']),
@@ -52,7 +48,6 @@ from drf_spectacular.utils import extend_schema, extend_schema_view,OpenApiRespo
 #     permission_classes=[permissions.AllowAny]
 #     serializer_class=QuestionSerializer
 
-
 # @extend_schema_view(
 #     list=extend_schema(tags=['Exam']),
 #     retrieve=extend_schema(tags=['Exam']),
@@ -66,159 +61,6 @@ from drf_spectacular.utils import extend_schema, extend_schema_view,OpenApiRespo
 #     permission_classes=[permissions.AllowAny]
 #     serializer_class=ExamSerializer
 
-
-####SUBJECT
-@extend_schema(
-    methods=['GET'],
-    responses={200:SubjectSerializer(many=True)}
-)
-@extend_schema(
-    methods=['POST'],
-    request=SubjectSerializer,
-    responses={
-        201:SubjectSerializer,
-        400: OpenApiResponse(description='Bad resquest')
-    }
-)
-@api_view(['GET','POST'])
-def subject_list(request):
-    if request.method=='GET':
-        subjects=Subject.objects.all()
-        serializer=SubjectSerializer(subjects,many=True)
-        return Response(serializer.data)
-
-    serializer=SubjectSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def subject_detail(request, pk):
-
-    try:
-        subject = Subject.objects.get(pk=pk)
-    except Subject.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SubjectSerializer(subject)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SubjectSerializer(subject, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        subject.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-######### TOPIC############
-@extend_schema(
-    methods=['GET'],
-    responses={200:TopicSerializer(many=True)}
-)
-@extend_schema(
-    methods=['POST'],
-    request=TopicSerializer,
-    responses={
-        201:TopicSerializer,
-        400: OpenApiResponse(description='Bad resquest')
-    }
-)
-@api_view(['GET','POST'])
-def topic_list(request):
-    if request.method=='GET':
-        topic=Topic.objects.all()
-        serializer=TopicSerializer(topic,many=True)
-        return Response(serializer.data)
-
-    serializer=TopicSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def topic_detail(request, pk):
-
-    try:
-        topic = Topic.objects.get(pk=pk)
-    except Topic.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = TopicSerializer(topic)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = TopicSerializer(topic, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        topic.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    
-
-####### QUEStION
-
-@extend_schema(
-    methods=['GET'],
-    responses={200:QuestionSerializer(many=True)}
-)
-@extend_schema(
-    methods=['POST'],
-    request=QuestionSerializer,
-    responses={
-        201:QuestionSerializer,
-        400: OpenApiResponse(description='Bad resquest')
-    }
-)
-@api_view(['GET','POST'])
-def question_list(request):
-    if request.method=='GET':
-        question=Question.objects.all()
-        serializer=QuestionSerializer(question,many=True)
-        return Response(serializer.data)
-
-    serializer=QuestionSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def question_detail(request, pk):
-
-    try:
-        question = Question.objects.get(pk=pk)
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = QuestionSerializer(question)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = QuestionSerializer(question, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        question.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
 
 ########## EXAMAN
 @extend_schema(
@@ -272,73 +114,3 @@ def exam_detail(request, pk):
     
     
     
-@api_view(['GET'])
-def teacher_subjects(request,teacher_id):
-    try:
-        teacher = Teacher.objects.get(pk=teacher_id)
-    except Teacher.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    subjects=teacher.subjects.all()
-    serializer=SubjectSerializer(subjects,many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
-
-@api_view(['GET'])
-def subject_questions(request,subject_id):
-    try:
-        subject = Subject.objects.get(pk=subject_id)
-    except Subject.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    questions=Question.objects.filter(subject=subject_id)
-    serializer=QuestionSerializer(questions,many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
-
-
-
-@api_view(['GET'])
-def topic_subject(request,topic_id):
-    try:
-        topic=Topic.objects.get(pk=topic_id)
-    except Topic.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-        
-    subject=topic.Subject
-    serializer=SubjectSerializer(subject)
-    return Response(serializer.data,status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def question_topic(request,question_id):
-    try:
-        question=Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-        
-    topic=question.topic
-    serializer=TopicSerializer(topic)
-    return Response(serializer.data,status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def subject_topics(request,subject_id):
-    try:
-        subject=Subject.objects.get(pk=subject_id)
-    except Subject.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-        
-    topic=Topic.objects.filter(Subject=subject)
-    serializer=TopicSerializer(topic,many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def topic_question(request,topic_id):
-    try:
-        topic=Topic.objects.get(pk=topic_id)
-    except Topic.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-        
-    questions=Question.objects.filter(topic=topic)
-    serializer=QuestionSerializer(questions,many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
